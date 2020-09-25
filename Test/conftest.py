0362dd774dfcc0e1637765c0afc34fd612d1d65d
ShreedharB-hub <Shreedharbeesanakoppa@gmail.com>
2020-09-25 15:05:01 +0530

    ChromeEdited

--- Test/conftest.py
+++ Test/conftest.py
@@ -1,10 +1,11 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="class")
 def setup(request):
-    driver = webdriver.Chrome(executable_path="F:\Softwares\Testing\chromedriver_win32\chromedriver.exe")
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://paytm.com/")
     driver.maximize_window()
     driver.delete_all_cookies()
